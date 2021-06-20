600830c473a6b1dd38798c60ae90059660799b11
clopezag <85889406+clopezag@users.noreply.github.com>
2021-06-20 18:49:48 +0200

    Logic modified to include a new condition (it should show "Buzz" if the parameter contains a five)

--- src/main/java/com/jcyl/BuzzConverter.java
+++ src/main/java/com/jcyl/BuzzConverter.java
@@ -2,7 +2,7 @@ package com.jcyl;
 
 public class BuzzConverter implements Converter{
   public boolean meetRule(int value) {
-    return value % 5 == 0;
+    return value % 5 == 0 || String.valueOf(value).contains(String.valueOf(5));
   }
   
   @Override
