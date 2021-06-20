3cd77bd9360c892a7be3ace6048c8668927e7362
clopezag <85889406+clopezag@users.noreply.github.com>
2021-06-20 18:42:07 +0200

    Logic modified to include a new condition (it should show "Fizz" if the parameter contains a three)

--- src/main/java/com/jcyl/FizzConverter.java
+++ src/main/java/com/jcyl/FizzConverter.java
@@ -3,7 +3,7 @@ package com.jcyl;
 public class FizzConverter implements Converter{
   public final int VALUE_MATCH= 3;
   public boolean meetRule(int value) {
-    return value % VALUE_MATCH == 0;
+    return value % VALUE_MATCH == 0 || String.valueOf(value).contains(String.valueOf(VALUE_MATCH));
   }
   
   @Override
